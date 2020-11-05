4be2e0bce8cd2bba0ce9a480f23c09a2a50bdef9
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2020-11-05 04:55:01 +0100

    Updated weather page

--- react-part1/src/components/WeatherPage.js
+++ react-part1/src/components/WeatherPage.js
@@ -58,124 +58,132 @@ function WeatherPage(props) {
 
                     <div className="my-3">
                         <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-                            <ToggleButton value={1} onClick={() => onBtnChangeHandler('Horsens')}>Horsens</ToggleButton>
-                            <ToggleButton value={2} onClick={() => onBtnChangeHandler('Aarhus')}>Århus</ToggleButton>
-                            <ToggleButton value={3} onClick={() => onBtnChangeHandler('Copenhagen')}>Copenhagen</ToggleButton>
+                            <ToggleButton variant="info" value={1} onClick={() => onBtnChangeHandler('Horsens')}>Horsens</ToggleButton>
+                            <ToggleButton variant="info" value={2} onClick={() => onBtnChangeHandler('Aarhus')}>Århus</ToggleButton>
+                            <ToggleButton variant="info" value={3} onClick={() => onBtnChangeHandler('Copenhagen')}>Copenhagen</ToggleButton>
                         </ToggleButtonGroup>
 
                         <br />
                     </div>
                     <div>
+<<<<<<< Updated upstream
                         <Filter selectedCity={selectedCity} triggerFilterSet = {handleCallback}/>
                     </div>
                     <div>   
                          <Button className="outline-btn mt-3" onClick={() => retrieveAllData(selectedCity)}>Reload data</Button>{' '}
+=======
+                        <Filter props={selectedCity} />
+                        <div><Button className="outline-btn mt-3" onClick={() => retrieveAllData(selectedCity)}>Reload data</Button>{' '}</div>
+>>>>>>> Stashed changes
                     </div>
                 </div>
 
             </Jumbotron>
             <Accordion>
-                <Card>
+                <Card className="myCard pt-3 mt-0">
                     <div className="row text-center mb-5">
-                        <div className="col-6">
-                            <Card className="my-card">
-                                <Accordion.Toggle as={Button} className="outline-link " eventKey="0">SEE WEATHER FORECAST</Accordion.Toggle>
-                            </Card>
+                        <div className="offset-md-3 col-md-3 text-center mb-2">
+                            <Accordion.Toggle as={Button} className="outline-link " eventKey="0">SEE WEATHER FORECAST</Accordion.Toggle>
+
                         </div>
-                        <div className="col-6">
-                            <Card className="my-card">
-                                <Accordion.Toggle as={Button} className="outline-link" eventKey="1">SEE WEATHER HISTORY</Accordion.Toggle>
-                            </Card>
+                        <div className="col-md-3">
+
+                            <Accordion.Toggle as={Button} className="outline-link" eventKey="1">SEE WEATHER HISTORY</Accordion.Toggle>
+
                         </div>
                     </div>
                     <Accordion.Collapse eventKey="0">
-                        <Card.Body> <Table id="weatherForecast">
-                            <thead className="text-center">
-                                <tr>
-                                    <th>Place</th>
-                                    <th>Type</th>
-                                    <th>From</th>
-                                    <th>To</th>
-                                    <th>Unit</th>
-                                    <th>time</th>
-                                    <th>Precipitation Type</th>
-                                    <th>Wind Direction</th>
+                        <Card.Body>
+                            <p className="text-center lead"> Weather Forecast For {selectedCity}</p>
+                            <Table id="weatherForecast" responsive striped bordered hover >
+                                <thead className="text-center">
+                                    <tr>
+                                        <th>Place</th>
+                                        <th>Type</th>
+                                        <th>From</th>
+                                        <th>To</th>
+                                        <th>Unit</th>
+                                        <th>time</th>
+                                        <th>Precipitation Type</th>
+                                        <th>Wind Direction</th>
 
-                                </tr>
-                            </thead>
-                            <tbody className="text-center">
-                                {forecastData.map((item, index) => {
-                                    if (item.type == 'precipitation') {
-                                        let precTypes = item.precipitation_types;
-                                        let str = "";
-                                        precTypes.forEach(x => {
-                                            if (x == "") {
-                                                str = x;
-                                            }
-                                            else {
-                                                str += " " + x + ", ";
-                                            }
-                                        })
-                                        return <tr key={index}>
-                                            <td>{item.place}</td>
-                                            <td>{item.type}</td>
-                                            <td>{item.from}</td>
-                                            <td>{item.to}</td>
-                                            <td>{item.unit}</td>
-                                            <td>{item.time}</td>
-                                            <td>{str}</td>
-                                            <td>-</td>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-center">
+                                    {forecastData.map((item, index) => {
+                                        if (item.type == 'precipitation') {
+                                            let precTypes = item.precipitation_types;
+                                            let str = "";
+                                            precTypes.forEach(x => {
+                                                if (x == "") {
+                                                    str = x;
+                                                }
+                                                else {
+                                                    str += " " + x + ", ";
+                                                }
+                                            })
+                                            return <tr key={index}>
+                                                <td>{item.place}</td>
+                                                <td>{item.type}</td>
+                                                <td>{item.from}</td>
+                                                <td>{item.to}</td>
+                                                <td>{item.unit}</td>
+                                                <td>{item.time}</td>
+                                                <td>{str}</td>
+                                                <td>-</td>
 
-                                        </tr>
-                                    }
-                                    else if (item.type == 'wind speed') {
-                                        let windDir = item.directions;
-                                        let str1 = "";
-                                        windDir.forEach(x => {
-                                            if (x == "") {
-                                                str1 = x;
-                                            }
-                                            else {
-                                                str1 += " " + x + ", ";
-                                            }
-                                        })
-                                        return <tr key={index}>
-                                            <td>{item.place}</td>
-                                            <td>{item.type}</td>
-                                            <td>{item.from}</td>
-                                            <td>{item.to}</td>
-                                            <td>{item.unit}</td>
-                                            <td>{item.time}</td>
-                                            <td>-</td>
-                                            <td>{str1}</td>
+                                            </tr>
+                                        }
+                                        else if (item.type == 'wind speed') {
+                                            let windDir = item.directions;
+                                            let str1 = "";
+                                            windDir.forEach(x => {
+                                                if (x == "") {
+                                                    str1 = x;
+                                                }
+                                                else {
+                                                    str1 += " " + x + ", ";
+                                                }
+                                            })
+                                            return <tr key={index}>
+                                                <td>{item.place}</td>
+                                                <td>{item.type}</td>
+                                                <td>{item.from}</td>
+                                                <td>{item.to}</td>
+                                                <td>{item.unit}</td>
+                                                <td>{item.time}</td>
+                                                <td>-</td>
+                                                <td>{str1}</td>
 
-                                        </tr>
-                                    }
-                                    else
-                                        return <tr key={index}>
-                                            <td>{item.place}</td>
-                                            <td>{item.type}</td>
-                                            <td>{item.from}</td>
-                                            <td>{item.to}</td>
-                                            <td>{item.unit}</td>
-                                            <td>{item.time}</td>
-                                            <td>-</td>
-                                            <td>-</td>
+                                            </tr>
+                                        }
+                                        else
+                                            return <tr key={index}>
+                                                <td>{item.place}</td>
+                                                <td>{item.type}</td>
+                                                <td>{item.from}</td>
+                                                <td>{item.to}</td>
+                                                <td>{item.unit}</td>
+                                                <td>{item.time}</td>
+                                                <td>-</td>
+                                                <td>-</td>
 
-                                        </tr>
-                                })}
+                                            </tr>
+                                    })}
 
 
-                            </tbody>
-                        </Table></Card.Body>
+                                </tbody>
+                            </Table></Card.Body>
                     </Accordion.Collapse>
                 </Card>
                 <Card>
 
                     <Accordion.Collapse eventKey="1">
                         <Card.Body>
+                            <p className="text-center lead"> Weather History For {selectedCity}</p>
 
-                            <Table id="weatherHistory">
+
+                            <Table id="weatherHistory" responsive striped bordered hover>
                                 <thead className="text-center">
                                     <tr>
                                         <th>Place</th>
