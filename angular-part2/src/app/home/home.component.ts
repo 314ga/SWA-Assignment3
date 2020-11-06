a33f67533dbef3a53504fc138a493d124d196c1c
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2020-11-06 04:58:19 +0100

    displaying data on table

--- angular-part2/src/app/home/home.component.ts
+++ angular-part2/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestAPIService } from '../rest-api.service'
+
 
 @Component({
   selector: 'app-home',
@@ -8,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   clickCounter: number = 0;
   name: string = '';
-  constructor() { }
+  weatherData: Object;
+
+  constructor(private restApi: RestAPIService) { }
 
   ngOnInit(): void {
+    this.restApi.getAllData("data").subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    });
+    // this.restApi.postHistoricData("temperature", 150, "C", new Date(), "Horsens", "").subscribe(data => {
+    //   this.succesResponse = data;
+    //   console.log(data);
+    // }) 
   }
   countClick() {
     this.clickCounter += 1;
