199a0f367b992b369bae37e6b50e255efabed96d
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2020-11-06 05:49:21 +0100

    Fetching weather
    Getting weather history by city with button click

--- angular-part2/src/app/home/home.component.ts
+++ angular-part2/src/app/home/home.component.ts
@@ -11,21 +11,33 @@ export class HomeComponent implements OnInit {
   clickCounter: number = 0;
   name: string = '';
   weatherData: Object;
+  fetchFrom: string = 'data';
+  fetchHistory: string = 'data';
+  fetchForecast: string = 'forecast';
 
   constructor(private restApi: RestAPIService) { }
 
   ngOnInit(): void {
-    this.restApi.getAllData("data").subscribe(data => {
-      this.weatherData = data;
-      console.log(this.weatherData);
-    });
-    // this.restApi.postHistoricData("temperature", 150, "C", new Date(), "Horsens", "").subscribe(data => {
-    //   this.succesResponse = data;
-    //   console.log(data);
-    // }) 
+    // this.restApi.getAllData(this.fetchHistory).subscribe(data => {
+    //   this.weatherData = data;
+    //   console.log(this.weatherData);
+    // });
+    // this.restApi.getAllData(this.fetchForecast).subscribe(data => {
+    //   this.weatherData = data;
+    //   console.log(this.weatherData);
+    // });
   }
   countClick() {
     this.clickCounter += 1;
   }
 
+  getWeatherData(place) {
+    this.fetchForecast = place;
+
+    this.restApi.getAllData(this.fetchHistory + "/" + place).subscribe(data => {
+      this.weatherData = data;
+      console.log(this.weatherData);
+    });
+
+  }
 }
--- angular-part2/src/app/material.ts
+++ angular-part2/src/app/material.ts
@@ -1,12 +1,15 @@
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from "@angular/core";
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 @NgModule({
-    imports: [MatInputModule, MatDialogModule, MatNativeDateModule, MatFormFieldModule, MatSliderModule, MatButtonModule],
-    exports: [MatInputModule, MatDialogModule, MatNativeDateModule, MatFormFieldModule, MatSliderModule, MatButtonModule],
+    imports: [MatInputModule, MatExpansionModule, MatButtonToggleModule, MatDialogModule, MatNativeDateModule, MatFormFieldModule, MatSliderModule, MatButtonModule],
+    exports: [MatInputModule, MatExpansionModule, MatButtonToggleModule, MatDialogModule, MatNativeDateModule, MatFormFieldModule, MatSliderModule, MatButtonModule],
 })
 export class MaterialModule { }
